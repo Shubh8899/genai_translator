923374de783f672a9480392223ca3b3a1d7d7cab
Shubham Chauhan <shubham.chauhan826@gmail.com>
2024-04-13 14:53:28 +0530

    Implemented translation service

--- src/translation/application/azure-translator.service.ts
+++ src/translation/application/azure-translator.service.ts
@@ -1,4 +1,51 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom, map } from 'rxjs';
+import { v4 } from 'uuid';
+import { TranslateInput } from './interfaces/translator-input.interface';
+import { Translator } from './interfaces/translator.interface';
+import { TranslationResult } from './interfaces/translator-result.interface';
+import { env } from 'src/configs/env.config';
+import { Integration } from 'src/core/types/integration.type';
+
+type AzureTranslateResponse = {
+  translations: [
+    {
+      text: string;
+      to: string;
+    },
+  ];
+};
 
 @Injectable()
-export class AzureTranslatorService {}
+export class AzureTranslatorService implements Translator {
+  constructor(private httpService: HttpService) {}
+
+  async translate({ text, srcLanguageCode, targetLanguageCode }: TranslateInput): Promise<TranslationResult> {
+    // https://learn.microsoft.com/en-us/azure/ai-services/translator/language-support
+    const data = [{ text }];
+    const result$ = this.httpService
+      .post<AzureTranslateResponse[]>(env.AZURE_OPENAI_TRANSLATOR.URL, data, {
+        headers: {
+          'Ocp-Apim-Subscription-Key': env.AZURE_OPENAI_TRANSLATOR.KEY,
+          'Ocp-Apim-Subscription-Region': env.AZURE_OPENAI_TRANSLATOR.LOCATION,
+          'Content-type': 'application/json',
+          'X-ClientTraceId': v4(),
+        },
+        params: {
+          'api-version': env.AZURE_OPENAI_TRANSLATOR.VERSION,
+          from: srcLanguageCode,
+          to: targetLanguageCode,
+        },
+        responseType: 'json',
+      })
+      .pipe(
+        map(({ data }) => data?.[0]?.translations?.[0].text || 'No result'),
+        map((text) => ({
+          text,
+          aiService: <Integration>'azureOpenAI',
+        })),
+      );
+    return firstValueFrom(result$);
+  }
+}
